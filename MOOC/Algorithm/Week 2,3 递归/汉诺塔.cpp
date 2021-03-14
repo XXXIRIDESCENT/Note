db0288ba3be869d0f8f467b436f4bf24981ac7b7
XXXIRIDESCENT <60588554+XXXIRIDESCENT@users.noreply.github.com>
2021-03-14 15:49:32 +0800

    Create 汉诺塔.cpp

---
+++ MOOC/Algorithm/Week 2,3 递归/汉诺塔.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void H(int n, char a, char b, char c)
+{
+	if (n == 1)
+		cout << a << "->" << c << endl;
+	else
+	{
+		H(n - 1, a, c, b);
+		cout << a << "->" << c << endl;
+		H(n - 1, b, a, c);
+	}
+}
+int main()
+{
+	int n;
+	cin >> n; 
+  H(n, 'A', 'B', 'C');
+}
